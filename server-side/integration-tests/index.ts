b61f0214b9f30c68e25db20fee9100ab5230e295
Noam Ben David <>
2023-08-07 17:58:36 +0300

    save progress

--- server-side/integration-tests/index.ts
+++ server-side/integration-tests/index.ts
@@ -1,4 +1,5 @@
 export * from "./test-commands/import-tests/import-data/small-size-data-command"
 export * from "./test-commands/import-tests/import-data/big-size-data-command"
+export * from "./test-commands/import-tests/import-data/import-not-existing-item-command"
 export * from "./test-commands/import-tests/import-file/small-file-command"
 export * from "./test-commands/import-tests/import-file/big-file-command"
---
+++ server-side/integration-tests/test-commands/import-tests/import-data/import-not-existing-item-command.ts
@@ -0,0 +1,35 @@
+import { Client } from "@pepperi-addons/debug-server/dist"
+import { ImportDataBaseCommand } from "./import-data-base-command"
+import { ItemRelations } from "shared";
+
+export class ImportNotExistingItemCommand extends ImportDataBaseCommand {
+    constructor(client: Client){
+        super(client, 'import-not-existing-item')
+    }
+
+    async initData(): Promise<ItemRelations[]> {
+        var relations: ItemRelations[] = [];
+        //item that contains item that not exist in its related items
+        let firstItem: ItemRelations = {
+            "CollectionName": this.collectionName,
+            "ItemExternalID": this.items[0].ExternalID,
+            "RelatedItems": [this.items[1].ExternalID, this.items[2].ExternalID, 'notExistingItem']
+        }
+        //item points to itself 
+        let secondItem: ItemRelations = {
+            "CollectionName": this.collectionName,
+            "ItemExternalID": this.items[1].ExternalID,
+            "RelatedItems": [this.items[1].ExternalID, this.items[2].ExternalID]
+        }  
+        relations.push(firstItem);
+        relations.push(secondItem);
+        return relations;
+    }
+
+    async test(res: any, data: any, expect: Chai.ExpectStatic): Promise<any> {
+        this.mockItemRelationsData.map(item => {
+            debugger
+            console.log(item)
+        });
+    }
+}
