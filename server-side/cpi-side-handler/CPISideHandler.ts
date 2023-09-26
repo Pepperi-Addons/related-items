2f53fac0cd694b91f191644517652129d85507af
Noam Ben David <>
2023-09-26 11:47:05 +0300

    save progress

--- server-side/cpi-side-handler/CPISideHandler.ts
+++ server-side/cpi-side-handler/CPISideHandler.ts
@@ -3,6 +3,7 @@ import { BatchApiResponse, PapiClient} from "@pepperi-addons/papi-sdk";
 import { ItemRelations, RELATED_ITEM_CPI_META_DATA_TABLE_NAME, RELATED_ITEM_META_DATA_TABLE_NAME } from "shared";
 import config from "../../addon.config.json";
 import { ItemsService } from "../items-service";
+import split from 'just-split';
 
 // this class converts item relations to cpi side format
 // cpi side format is to use uuids instead of external ids (for items)
@@ -11,7 +12,7 @@ export class CPISideHanler {
     }
 
     async handlePNS(body: any) {
-        const itemsRelationsKeys = body.Message.ModifiedObjects;
+        const itemsRelationsKeys = body.Message.ModifiedObjects.map(obj => obj.ObjectKey);
         // the keys are in the format of: "{CollectionName}_ItemExternalID"
         console.log("itemsRelationsKeys: ", itemsRelationsKeys);
 
@@ -33,7 +34,10 @@ export class CPISideHanler {
 
     // batch update related items table for CPI
     private async updateRelatedItemsTable(itemsRelations: ItemRelations[]): Promise<BatchApiResponse[]> {
-        return await this.papiClient.addons.data.uuid(config.AddonUUID).table(RELATED_ITEM_CPI_META_DATA_TABLE_NAME).batch(itemsRelations);
+        debugger
+        console.log(`updateRelatedItemsTable itemsRelations: ${JSON.stringify(itemsRelations)}`);
+        return await this.papiClient.post('addons/data/batch/config.AddonUUID/RELATED_ITEM_CPI_META_DATA_TABLE_NAME', itemsRelations);
+       // return await this.papiClient.addons.data.uuid(config.AddonUUID).table(RELATED_ITEM_CPI_META_DATA_TABLE_NAME).batch(itemsRelations);
     }
 
     // get all items mapped by external id
@@ -41,8 +45,10 @@ export class CPISideHanler {
         const itemsService = new ItemsService(this.papiClient);
         const itemsMap = await itemsService.getItemsByExternalID(externalIdsArray, [
             "ExternalID",
-            "UUID"
+            "InternalID"
         ], "ExternalID");
+
+        console.log(`itemsMap: ${JSON.stringify(itemsMap)}`);
 
         return itemsMap;
     }
@@ -53,13 +59,13 @@ export class CPISideHanler {
     private convertToCPIFormat(itemsRelations: ItemRelations[], items: Map<string, any>): ItemRelations[] {
         const itemsRelationsForCPI = itemsRelations.map(itemRelation => {
             // get primary item uuid
-            const itemUUID = items.get(itemRelation.ItemExternalID!).UUID;
+            const itemUUID = items.get(itemRelation.ItemExternalID!).InternalID;
             if (!itemUUID) {
                 console.error(`itemUUID was not found for itemExternalID: ${itemRelation.ItemExternalID}`);
                 return undefined;
             }
             // get related items uuids
-            const relatedItemsUUIDs = itemRelation.RelatedItems?.map(ri => items.get(ri).UUID);
+            const relatedItemsUUIDs = itemRelation.RelatedItems?.map(ri => items.get(ri).InternalID);
             console.log(`@@@itemUUID: ${itemUUID} relatedItemsUUIDs: ${relatedItemsUUIDs}`);
             const key = `${itemRelation.CollectionName}_${itemUUID}`;
             const cpiRelationItem = { 'Key': key, 'Hidden': itemRelation.Hidden, RelatedItems: relatedItemsUUIDs }
@@ -67,14 +73,15 @@ export class CPISideHanler {
             return cpiRelationItem as ItemRelations;
         });
 
+        console.log(`@@@itemsRelationsForCPI: ${JSON.stringify(itemsRelationsForCPI)}`);
         const res = itemsRelationsForCPI.filter(item => item !== undefined) as ItemRelations[];
         return res;
     }
     // get related items object by key list
     private async getRelatedItemsByKeyList(keyList: string[]): Promise<ItemRelations[]> {
-        const itemRelations: ItemRelations[] = await this.papiClient.addons.data.search.uuid(config.AddonUUID).table(RELATED_ITEM_META_DATA_TABLE_NAME).post({KeyList: keyList}) as any;
-        console.log(`getRelatedItemsByKeyList itemRelations: ${JSON.stringify(itemRelations)}`);
-        return itemRelations;
+        const itemRelations = await this.papiClient.addons.data.search.uuid(config.AddonUUID).table(RELATED_ITEM_META_DATA_TABLE_NAME).post({KeyList: keyList}) as any;
+        console.log(`getRelatedItemsByKeyList itemRelations: ${JSON.stringify(itemRelations.Objects)}`);
+        return itemRelations.Objects;
     }
 
     // creates an string array of all the items external ids and related items external ids
